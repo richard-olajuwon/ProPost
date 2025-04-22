8d32990ff2e61e3e30d63211a9d7d7769c5751c1
SCOPE <olajuwonrichard22@gmail.com>
2025-04-22 12:24:09 +0100

    fixed redundant images being displayed in the build file

--- frontend/src/pages/TopicBlog.jsx
+++ frontend/src/pages/TopicBlog.jsx
@@ -40,14 +40,6 @@ const TopicBlog = () => {
             )}
           </div>
 
-          <img
-            src="./src/assets/images/shadow-3.svg"
-            width="500"
-            height="1500"
-            loading="lazy"
-            alt=""
-            className="feature-bg"
-          />
         </section>
         <BlogPost></BlogPost>
         <Footer></Footer>
